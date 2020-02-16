672467f3e6f6d2345bc26cc461cced4382fc48b8
Michael Ochnicki <ochnicki.michael@gmail.com>
2020-02-16 06:43:00 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,6 +1,8 @@
 def square_array(array)
   count = 0
   
-  while count < array.length
-    
+  while count < array.length do
+    array[count] = array[count] * array[count]
+    count += 1
+  end
 end
